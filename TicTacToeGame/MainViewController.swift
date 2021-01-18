abd8a677a0c78110a84f4b73f99aa95f997e5e53
Anastasiia Zubkova <stasiya.mail@gmail.com>
2021-01-17 20:16:44 -0500

    Added signs drawn when tapped Checks for tap in a taken cell Checks for all cells taken (end of the game)

--- TicTacToeGame/MainViewController.swift
+++ TicTacToeGame/MainViewController.swift
@@ -26,37 +26,139 @@ class MainViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+    }
+    
+    func getSign() -> String {
+        if chosen {return "Cross"}
+        else {return "Circle"}
+    }
+    
+    func isAllDone() -> Bool {
+        if (zeroZero.image(for: .normal) != nil &&
+            zeroOne.image(for: .normal) != nil &&
+            zeroTwo.image(for: .normal) != nil &&
+            oneZero.image(for: .normal) != nil &&
+            oneOne.image(for: .normal) != nil &&
+            oneTwo.image(for: .normal) != nil &&
+            twoZero.image(for: .normal) != nil &&
+            twoOne.image(for: .normal) != nil &&
+            twoTwo.image(for: .normal) != nil)
+        {
+            return true
+        }
+        else {
+            return false
+        }
     }
     
     
     @IBAction func zeroZeroTapped(_ sender: Any) {
+        if(zeroZero.image(for: .normal) == nil){
+            zeroZero.setImage( UIImage.init(named: getSign()), for: .normal)
+        }
+        else{
+            print("This cell has a sign already, try another one")
+        }
+        if (isAllDone()){
+            print("Game Ended")
+        }
         
     }
     
     @IBAction func zeroOneTapped(_ sender: Any) {
-        
+        if(zeroOne.image(for: .normal) == nil){
+            zeroOne.setImage( UIImage.init(named: getSign()), for: .normal)
+        }
+        else{
+            print("This cell has a sign already, try another one")
+        }
+        if (isAllDone()){
+            print("Game Ended")
+        }
     }
     
     @IBAction func zeroTwoTapped(_ sender: Any) {
+        if(zeroTwo.image(for: .normal) == nil){
+            zeroTwo.setImage( UIImage.init(named: getSign()), for: .normal)
+        }
+        else{
+            print("This cell has a sign already, try another one")
+        }
+        if (isAllDone()){
+            print("Game Ended")
+        }
     }
     
     @IBAction func oneZeroTapped(_ sender: Any) {
+        if(oneZero.image(for: .normal) == nil){
+            oneZero.setImage( UIImage.init(named: getSign()), for: .normal)
+        }
+        else{
+            print("This cell has a sign already, try another one")
+        }
+        if (isAllDone()){
+            print("Game Ended")
+        }
     }
     
     @IBAction func oneOneTapped(_ sender: Any) {
+        if(oneOne.image(for: .normal) == nil){
+            oneOne.setImage( UIImage.init(named: getSign()), for: .normal)
+        }
+        else{
+            print("This cell has a sign already, try another one")
+        }
+        if (isAllDone()){
+            print("Game Ended")
+        }
     }
     
     @IBAction func oneTwoTapped(_ sender: Any) {
+        if(oneTwo.image(for: .normal) == nil){
+            oneTwo.setImage( UIImage.init(named: getSign()), for: .normal)
+        }
+        else{
+            print("This cell has a sign already, try another one")
+        }
+        if (isAllDone()){
+            print("Game Ended")
+        }
     }
     
     @IBAction func twoZeroTapped(_ sender: Any) {
+        if(twoZero.image(for: .normal) == nil){
+            twoZero.setImage( UIImage.init(named: getSign()), for: .normal)
+        }
+        else{
+            print("This cell has a sign already, try another one")
+        }
+        if (isAllDone()){
+            print("Game Ended")
+        }
     }
     
     @IBAction func twoOneTapped(_ sender: Any) {
+        if(twoOne.image(for: .normal) == nil){
+            twoOne.setImage( UIImage.init(named: getSign()), for: .normal)
+        }
+        else{
+            print("This cell has a sign already, try another one")
+        }
+        if (isAllDone()){
+            print("Game Ended")
+        }
     }
     
     @IBAction func twoTwoTapped(_ sender: Any) {
+        if(twoTwo.image(for: .normal) == nil){
+            twoTwo.setImage( UIImage.init(named: getSign()), for: .normal)
+        }
+        else{
+            print("This cell has a sign already, try another one")
+        }
+        if (isAllDone()){
+            print("Game Ended")
+        }
     }
 }
