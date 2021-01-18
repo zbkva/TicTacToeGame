3898a8f883276eae669cf48d45e1aaa80d3a4ff7
Anastasiia Zubkova <stasiya.mail@gmail.com>
2021-01-17 21:17:35 -0500

    Added computer's move

--- TicTacToeGame/MainViewController.swift
+++ TicTacToeGame/MainViewController.swift
@@ -34,6 +34,99 @@ class MainViewController: UIViewController {
         else {return "Circle"}
     }
     
+    func getComputersSign() -> String {
+        if !chosen {return "Cross"}
+        else {return "Circle"}
+    }
+    func getRand() -> Int {
+        return Int.random(in: 1...9)
+    }
+    
+    func computersMove() {
+        let rand = getRand()
+        if(rand == 1){
+            if(zeroZero.image(for: .normal) == nil){
+                zeroZero.setImage( UIImage.init(named: getComputersSign()), for: .normal)
+            }
+            else {
+                computersMove()
+            }
+            
+        }
+        if(rand == 2){
+            if(zeroOne.image(for: .normal) == nil){
+                zeroOne.setImage( UIImage.init(named: getComputersSign()), for: .normal)
+            }
+            else {
+                computersMove()
+            }
+            
+        }
+        if(rand == 3){
+            if(zeroTwo.image(for: .normal) == nil){
+                zeroTwo.setImage( UIImage.init(named: getComputersSign()), for: .normal)
+            }
+            else {
+                computersMove()
+            }
+            
+        }
+        if(rand == 4){
+            if(oneZero.image(for: .normal) == nil){
+                oneZero.setImage( UIImage.init(named: getComputersSign()), for: .normal)
+            }
+            else {
+                computersMove()
+            }
+            
+        }
+        if(rand == 5){
+            if(oneOne.image(for: .normal) == nil){
+                oneOne.setImage( UIImage.init(named: getComputersSign()), for: .normal)
+            }
+            else {
+                computersMove()
+            }
+            
+        }
+        if(rand == 6){
+            if(oneTwo.image(for: .normal) == nil){
+                oneTwo.setImage( UIImage.init(named: getComputersSign()), for: .normal)
+            }
+            else {
+                computersMove()
+            }
+            
+        }
+        if(rand == 7){
+            if(twoZero.image(for: .normal) == nil){
+                twoZero.setImage( UIImage.init(named: getComputersSign()), for: .normal)
+            }
+            else {
+                computersMove()
+            }
+            
+        }
+        if(rand == 8){
+            if(twoOne.image(for: .normal) == nil){
+                twoOne.setImage( UIImage.init(named: getComputersSign()), for: .normal)
+            }
+            else {
+                computersMove()
+            }
+            
+        }
+        if(rand == 9){
+            if(twoTwo.image(for: .normal) == nil){
+                twoTwo.setImage( UIImage.init(named: getComputersSign()), for: .normal)
+            }
+            else {
+                computersMove()
+            }
+            
+        }
+    }
+    
     func isAllDone() -> Bool {
         if (zeroZero.image(for: .normal) != nil &&
             zeroOne.image(for: .normal) != nil &&
@@ -56,6 +149,7 @@ class MainViewController: UIViewController {
     @IBAction func zeroZeroTapped(_ sender: Any) {
         if(zeroZero.image(for: .normal) == nil){
             zeroZero.setImage( UIImage.init(named: getSign()), for: .normal)
+            computersMove()
         }
         else{
             print("This cell has a sign already, try another one")
@@ -69,6 +163,7 @@ class MainViewController: UIViewController {
     @IBAction func zeroOneTapped(_ sender: Any) {
         if(zeroOne.image(for: .normal) == nil){
             zeroOne.setImage( UIImage.init(named: getSign()), for: .normal)
+            computersMove()
         }
         else{
             print("This cell has a sign already, try another one")
@@ -81,6 +176,7 @@ class MainViewController: UIViewController {
     @IBAction func zeroTwoTapped(_ sender: Any) {
         if(zeroTwo.image(for: .normal) == nil){
             zeroTwo.setImage( UIImage.init(named: getSign()), for: .normal)
+            computersMove()
         }
         else{
             print("This cell has a sign already, try another one")
@@ -93,6 +189,7 @@ class MainViewController: UIViewController {
     @IBAction func oneZeroTapped(_ sender: Any) {
         if(oneZero.image(for: .normal) == nil){
             oneZero.setImage( UIImage.init(named: getSign()), for: .normal)
+            computersMove()
         }
         else{
             print("This cell has a sign already, try another one")
@@ -105,6 +202,7 @@ class MainViewController: UIViewController {
     @IBAction func oneOneTapped(_ sender: Any) {
         if(oneOne.image(for: .normal) == nil){
             oneOne.setImage( UIImage.init(named: getSign()), for: .normal)
+            computersMove()
         }
         else{
             print("This cell has a sign already, try another one")
@@ -117,6 +215,7 @@ class MainViewController: UIViewController {
     @IBAction func oneTwoTapped(_ sender: Any) {
         if(oneTwo.image(for: .normal) == nil){
             oneTwo.setImage( UIImage.init(named: getSign()), for: .normal)
+            computersMove()
         }
         else{
             print("This cell has a sign already, try another one")
@@ -129,6 +228,7 @@ class MainViewController: UIViewController {
     @IBAction func twoZeroTapped(_ sender: Any) {
         if(twoZero.image(for: .normal) == nil){
             twoZero.setImage( UIImage.init(named: getSign()), for: .normal)
+            computersMove()
         }
         else{
             print("This cell has a sign already, try another one")
@@ -141,6 +241,7 @@ class MainViewController: UIViewController {
     @IBAction func twoOneTapped(_ sender: Any) {
         if(twoOne.image(for: .normal) == nil){
             twoOne.setImage( UIImage.init(named: getSign()), for: .normal)
+            computersMove()
         }
         else{
             print("This cell has a sign already, try another one")
@@ -153,6 +254,7 @@ class MainViewController: UIViewController {
     @IBAction func twoTwoTapped(_ sender: Any) {
         if(twoTwo.image(for: .normal) == nil){
             twoTwo.setImage( UIImage.init(named: getSign()), for: .normal)
+            computersMove()
         }
         else{
             print("This cell has a sign already, try another one")
